0418837ed4c7b34bbd85cbb171e2c5d635383acd
Volodymyr <salashchenkovladymyr@gmail.com>
2025-11-22 19:17:36 +0200

    creared js, add libraries (axios,loader, simple, toast)

---
+++ src/js/render-functions.js
@@ -0,0 +1,36 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+//Ця функція повинна приймати масив images, 
+// створювати HTML-розмітку для галереї, 
+// додавати її в контейнер галереї 
+// та викликати метод екземпляра SimpleLightbox refresh(). 
+// Нічого не повертає.
+createGallery(images);
+
+
+
+//Ця функція нічого не приймає та повинна очищати вміст контейнера галереї. 
+// Нічого не повертає.
+
+clearGallery();
+
+
+
+//Ця функція нічого не приймає, 
+// повинна додавати клас для відображення лоадера. 
+// Нічого не повертає.//
+showLoader(); 
+
+
+
+
+//Ця функція нічого не приймає, 
+// повинна прибирати клас для відображення лоадера. 
+// Нічого не повертає.
+
+hideLoader(); 
---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+
+
+
+//import foo
+
+import getImagesByQuery from './js/pixabay-api';
+import createGallery from './js/render-functions';
+
+
+
+//logic foo
