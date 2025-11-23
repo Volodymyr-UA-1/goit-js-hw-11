644ff797da6e0027967487bb962bab172b655836
Volodymyr <salashchenkovladymyr@gmail.com>
2025-11-23 17:18:54 +0200

    add api.js+form.css

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,10 +1,20 @@
 import axios from "axios";
 
 
+
 //Ця функція повинна приймати один параметр query (пошукове слово, яке є рядком), 
 // здійснювати HTTP-запит і повертати значення властивості data з отриманої відповіді.
 
-
 export function getImagesByQuery(query){
-    
+const baseURL = 'https://pixabay.com';
+const endPoint ='/api/';
+const params = new URLSearchParams({
+key:"53366828-bd78e2a0684a00ed0fbe9f50e",
+q: query,
+image_type: 'photo',
+orientation: 'horizontal',
+safesearch: 'true',
+});
+const url = `${baseURL}${endPoint}?${params}`;
+return axios.get(url).then(res => res.data);
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,48 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-
-//import foo 
 import {getImagesByQuery} from './js/pixabay-api';
-import {createGallery,
-        clearGallery,
-        showLoader,
-        hideLoader,
-} from './js/render-functions';
+const formPixabay = document.querySelector('.form');
+const input = document.querySelector('input[name="search-text"]');
+formPixabay.addEventListener('submit', (e) =>{
+        e.preventDefault();
+const inputValue = input.value.trim();
+input.value = "";
+input.focus();
+
+ if (inputValue === "") {
+    iziToast.error({
+      message: 'Please enter text!',
+      position: 'topCenter',
+    });
+    return;
+  }
+  getImagesByQuery(inputValue)
+    .then(res => {
+      const hits = res.hits;
+      if (hits.length === 0) {
+        iziToast.error({
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+          position: 'bottomRight',
+          backgroundColor: 'red',
+          timeout: 3000,    
+          pauseOnHover: true, 
+          messageColor: 'white',
+        });
+        return;
+      }
+      console.log(hits);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+})
+// import {createGallery,
+//         clearGallery,
+//         showLoader,
+//         hideLoader,
+// } from './js/render-functions';
 
 
-//logic foo
+        // Імпортуй в нього функції із файлів pixabay-api.js та render-functions.js та викликай їх у відповідний момент.
+        // Імпортуй в нього функції із файлів pixabay-api.js та render-functions.js та викликай їх у відповідний момент. 
